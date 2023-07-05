70ec0fe78655271b1e8c8a67984b0a17a12bed28
Elvis <s49188705@gmail.com>
2023-07-05 13:08:57 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,7 +49,7 @@ module.exports = {
       colors: {
         headingColor: "#2e2e2e",
         textColor: "#515151",
-        cartNumBg: 'e80013',
+        cartNumBg: '#e80013',
         primary: "f5f3f3",
 
       },
